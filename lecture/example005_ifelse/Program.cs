5be2294cf1f5bc4e532cd93a976308ad56311d07
Калсын Черивов <Kals@DKS.local>
2022-08-22 16:44:05 +0300

    ifelse

---
+++ lecture/example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("User Name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "kals")
+{
+    Console.WriteLine("Nice, it is Kals");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
