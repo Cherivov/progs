9ef0e4d342919ecc3310b154252b232fa77c424c
Kals <cherivov@mail.ru>
2022-08-23 22:49:30 +0300

    Task 2

---
+++ seminar_001/task_02/Program.cs
@@ -0,0 +1,20 @@
+﻿int a, b, c;
+string input;
+
+Console.Write("Введите число №1: ");
+input = Console.ReadLine();
+a = Int32.Parse(input);
+
+Console.Write("Введите число №2: ");
+input = Console.ReadLine();
+b = Int32.Parse(input);
+
+Console.Write("Введите число №3: ");
+input = Console.ReadLine();
+c = Int32.Parse(input);
+
+int max = a;
+
+if (b>max) max = b;
+if (c>max) max = c;
+Console.Write("Максимальное число = " + max);
