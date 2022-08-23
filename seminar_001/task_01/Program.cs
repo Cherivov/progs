2926dd37958ce8e9abe9e04c3471d4941751d08f
Kals <cherivov@mail.ru>
2022-08-23 22:49:08 +0300

    Task 1 isprav

--- seminar_001/task_01/Program.cs
+++ seminar_001/task_01/Program.cs
@@ -9,14 +9,11 @@ Console.Write("Введите число №2: ");
 input = Console.ReadLine();
 b = Int32.Parse(input);
 
-int max = 0;
-
 if (a>b)
 {
-    max = a;
+    Console.WriteLine("Число №1 которое равно: " + a + " больше, числа №2 которое равно: " + b);    
 }
 else
 {
-    max = b;
+    Console.WriteLine("Число №2 которое равно: " + b + " больше, числа №1 которое равно: " + a);
 }
-Console.Write("Max = " + max);
