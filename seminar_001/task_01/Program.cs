cd186b141ef5e1922e52dd7b20ca7df2a5978c11
Kals <cherivov@mail.ru>
2022-08-23 22:36:06 +0300

    Task 1

---
+++ seminar_001/task_01/Program.cs
@@ -0,0 +1,22 @@
+﻿int a, b;
+string input;
+
+Console.Write("Введите число №1: ");
+input = Console.ReadLine();
+a = Int32.Parse(input);
+
+Console.Write("Введите число №2: ");
+input = Console.ReadLine();
+b = Int32.Parse(input);
+
+int max = 0;
+
+if (a>b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+Console.Write("Max = " + max);
