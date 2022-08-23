e379585d7fbff81aee19473c1251cb44cfa43318
Kals <cherivov@mail.ru>
2022-08-23 23:30:26 +0300

    Task 4

---
+++ seminar_001/task_04/Program.cs
@@ -0,0 +1,17 @@
+﻿int a, count;
+string input;
+
+Console.Write("Введите число: ");
+input = Console.ReadLine();
+a = Int32.Parse(input);
+
+count = 1;
+
+while (count <= a)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(count + ", ");
+    }
+    count = count + 1;
+}
