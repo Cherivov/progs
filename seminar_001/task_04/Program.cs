b166503e12191af024bffd8f4d2c55dccf40c0ff
Kals <cherivov@mail.ru>
2022-08-24 11:04:31 +0300

    Убрали запятую из вывода

---
+++ seminar_001/task_04/Program.cs
@@ -0,0 +1,29 @@
+﻿int a, count;
+string input;
+
+Console.Write("Введите число: ");
+input = Console.ReadLine();
+a = Int32.Parse(input);
+
+count = 1;
+
+while (count <= a)
+// Первый вариант:
+// {
+//     if (count % 2 == 0)
+//     {
+//         Console.Write(count + ", ");
+//     }
+//     count = count + 1;
+// }
+
+// Вариант с исправлением.
+{
+    if (count % 2 == 0)
+    {
+        if (count == a || count == a - 1) Console.Write(count);
+        else Console.Write(count + ", ");
+        
+    }
+    count = count + 1;
+}
