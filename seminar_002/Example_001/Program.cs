5cd61785d246d4eda41c79eac66241c976da8a8e
Cherivov <37274380+Cherivov@users.noreply.github.com>
2022-08-26 11:14:21 +0300

    Delete seminar_002/Example_001 directory

--- seminar_002/Example_001/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿int start = 100;
-int stop = 999;
-
-int number = new Random().Next(start, stop + 1);
-int digit_second = (number % 100) / 10;
-
-Console.WriteLine("Сгенерированное число: " + number);
-Console.WriteLine("Вторая цифра числа " + number + ": " + digit_second);
