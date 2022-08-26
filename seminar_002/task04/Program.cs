2506a3a941174b338959f522838892f782c40254
Kals <cherivov@mail.ru>
2022-08-26 18:07:56 +0300

    Task4

---
+++ seminar_002/task04/Program.cs
@@ -0,0 +1,14 @@
+﻿int start = 1;
+int stop = 100000;
+// int number = 162;            // Для проверки кода.
+int number = new Random().Next(start, stop + 1);
+Console.WriteLine("Сгенерированное число: " + number);
+
+if ((number % 7 == 0) & (number % 23 == 0))
+{
+    Console.WriteLine("Число " + number + " кратно 7 и 23!");
+}
+else
+{
+    Console.WriteLine("Число не кратно!");
+}
