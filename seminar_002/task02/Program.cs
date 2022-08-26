4acad29291b5bc1dd9ee4c32009f4009ecdefcd8
Kals <cherivov@mail.ru>
2022-08-26 22:57:03 +0300

    1

---
+++ seminar_002/task02/Program.cs
@@ -0,0 +1,38 @@
+﻿int start = 1;
+int stop = 65536;
+int digit_third;
+
+int number = new Random().Next(start, stop + 1);
+Console.WriteLine("Сгенерированное число: " + number);
+
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
+else
+{
+
+    if (number < 1000)
+    {
+        digit_third = number % 10;
+        Console.WriteLine("Третья цифра числа " + number + ": " + digit_third);
+    }
+    else
+    {
+        if (number < 10000)
+        {
+            digit_third = (number % 100) / 10;
+            Console.WriteLine("Третья цифра числа " + number + ": " + digit_third);
+        }
+
+        else
+
+        {
+
+
+            digit_third = (number % 1000) / 100;
+            Console.WriteLine("Третья цифра числа " + number + ": " + digit_third);
+
+        }
+    }
+}
---
+++ seminar_002/task03/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите цифру от 1 до 7:");
+int digit=int.Parse(Console.ReadLine());
+
+switch(digit)
+{
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine("Не выходной");
+        break;
+    case 6:
+    case 7:
+        Console.WriteLine("Выходной");
+        break;
+    default:
+        Console.WriteLine("Я вас не пониаю");
+        break;
+
+}
