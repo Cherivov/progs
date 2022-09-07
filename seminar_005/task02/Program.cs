4f07c965c8d8dd487125de2e18c8e2fe570635d3
Kals <cherivov@mail.ru>
2022-09-07 14:55:58 +0300

    task02

---
+++ seminar_005/task02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму четных элементов, стоящих на нечётных позициях (позиция - индекс элемента в массиве).
+
+Console.WriteLine("Введите длину массива: ");
+int L = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int[] array = new int[L];
+
+for (int i = 0; i < L; i++)
+{
+    int number = new Random().Next();
+    array[i] = number;
+    Console.WriteLine(array[i] + " ");
+    if ((array[i] % 2 == 0) && (i % 2 == 1))
+    {
+        sum=sum+array[i];
+    }
+}
+Console.WriteLine($"Сумма четных элементов, стоящих на нечётных позициях: {sum}");
