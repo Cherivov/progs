71cf47a9fb44f362d9fef14716e480b7bd1a74aa
Kals <cherivov@mail.ru>
2022-09-13 15:48:15 +0300

    task03_ispravl

--- seminar_007/task03/Program.cs
+++ seminar_007/task03/Program.cs
@@ -1,28 +1,28 @@
 ﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-int[,] array = new int[4, 4];
-double[] avg_array = new double[array.GetLength(0)];
-
-for (int j = 0; j < array.GetLength(0); j++)
+int n = 3;
+int m = 2;
+int[,] array = new int[n, m];
+double[] avg_array = new double[m];
+Random number = new Random();
+for (int j = 0; j < m; j++)
 {
     double sum = 0;
-    for (int i = 0; i < array.GetLength(1); i++)
+    for (int i = 0; i < n; i++)
     {
-        int number = new Random().Next(1, 100);
-        array[i, j] = number;
-        sum = sum + number;
+        array[i, j] = number.Next(1, 100);
+        sum = sum + array[i, j];
     }
-    avg_array[j] = sum / array.GetLength(0);
+    avg_array[j] = sum / n;
 }
-
-for (int i = 0; i < array.GetLength(0); i++)
+for (int i = 0; i < n; i++)
 {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int j = 0; j < m; j++)
     {
         Console.Write(array[i, j] + " ");
     }
     Console.WriteLine();
 }
-
+Console.WriteLine();
 for (int i = 0; i < avg_array.Length; i++)
 {
     Console.Write(avg_array[i] + " ");
