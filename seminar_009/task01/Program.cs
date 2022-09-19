a1fa33e62f930404c0b6cf3c51b43c002b43775d
Kals <cherivov@mail.ru>
2022-09-19 18:02:15 +0300

    task01

---
+++ seminar_009/task01/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите два параметра больше нуля: ");
+int firstParametr = Convert.ToInt32(Console.ReadLine());
+int secondParametr = Convert.ToInt32(Console.ReadLine());
+
+int Ackerman(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return Ackerman(n - 1, 1);
+    else
+        return Ackerman(n - 1, Ackerman(n, m - 1));
+}
+
+if ((firstParametr < 0) || (secondParametr < 0))
+{
+    Console.WriteLine("Вы ввели отрицательное число");
+}
+else
+{
+    int result = Ackerman(firstParametr, secondParametr);
+    Console.WriteLine(result);
+}
